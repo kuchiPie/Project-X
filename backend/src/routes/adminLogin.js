a38824440c9bac8524eaa758258283111290b2df
kuchiPie <shreyasbulbule007@gmail.com>
2022-11-14 02:21:00 +0530

    added three layer architecture

--- backend/src/routes/adminLogin.js
+++ backend/src/routes/adminLogin.js
@@ -1,72 +1,7 @@
 import { Router } from 'express';
 const router = new Router();
-import Admin from '../models/Admin.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import config from 'config';
+import adminLoginController from '../controllers/adminLoginController.js';
 
-//Load Input Validation
-import validateLoginInput from '../validation/login.js';
-
-//Post Router api/admin/login
-router.post('/admin/login', (req, res) => {
-    //Login Validation
-    const {
-        errors,
-        isValid
-    } = validateLoginInput(req.body);
-
-    //Check Validation
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-
-    const email = req.body.email;
-    const password = req.body.password;
-
-    //Find User By Email
-    Admin.findOne({
-        email:email
-    }).then(user => {
-        //Check if Your Exists
-        if (!user) {
-            return res.status(404).json({
-                emailNotFound: "Email is not registered"
-            });
-        }
-
-        // Check Hash
-        var isPasswordCorrect = bcrypt.compareSync(password, user.password);
-
-        // If password is incorrect send Error 400
-        if (isPasswordCorrect == false){
-            return res.status(400).json({
-                passwordIncorrect: "Password incorrect"
-            });
-        }
-        
-        // Payload for the token
-        const payload = {
-            id: user.id,
-            name: user.name
-        };
-
-        // Generating jwt token
-        var token = jwt.sign(payload, process.env.SECRET,{expiresIn:172800});
-
-        // Assiginig jwt token to user object
-        user.token = token
-
-        // Updating latest token to database
-        await user.save()
-
-        // returning final json as response
-        res.json({
-            success: true,
-            token: "Bearer" + token,
-            user: user.name
-        });
-    });
-});
+router.post('/admin/login', adminLoginController)
 
 export default router
