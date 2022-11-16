66bf31ba0cfb4a8bb5c7adccb3ed2a40de4ea701
Omkar Gowda <omkarkgowda@gmail.com>
2022-11-17 00:22:01 +0530

    "made student portal routes"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Admin from './pages/admin/Admin'
 import Student from './pages/student/Student'
+import Outpass from './pages/student/Outpass'
+import Profile from './pages/student/Profile'
 import Faculty from './pages/faculty/Faculty'
 import Login from './pages/Login'
 import Sessions from './pages/admin/Sessions'
@@ -15,6 +17,10 @@ function App(){
                     <Route path="manage_sessions" element={<Sessions/>}></Route>
                     <Route path='manage_faculty' element={<Faculty/>}/>
                     <Route path='manage_student' element={<Student/>}/>
+                </Route>
+                <Route path='/student' element={<Student/>}>
+                    <Route path="profile" element={<Profile/>}></Route>
+                    <Route path='outpass' element={<Outpass/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
