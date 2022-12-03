7c13bc23312dd3c1175496d96d80e7525d9b5536
Decoy101 <82807218+Decoy101@users.noreply.github.com>
2022-12-03 13:36:54 +0530

    admin section: Made changes to the Manage Faculty page, solved the reported issues

---
+++ frontend/src/pages/admin/components/EditFaculty.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { useState } from "react";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+
+import { useDispatch, useSelector } from "react-redux";
+import { editFacultyServer } from "../../../reduxSlices/FacultySlice";
+import { Dropdown } from "primereact/dropdown";
+import { classNames } from "primereact/utils";
+import { InputText } from "primereact/inputtext";
+
+const EditFaculty = ({rowData}) => {
+
+    let emptyFaculty = {
+        name: "",
+        institute_id: "",
+        department: "",
+        mentees: [],
+      };
+
+      const departments = [
+        {
+          label:"CSE",
+        },
+        {
+          label:"DSAI"
+        },
+        {
+          label:"ECE"
+        }
+      ] 
+
+    const [facultyInfoDialog,setFacultyInfoDialog] = useState(false)
+    const [faculty,setFaculty] = useState(emptyFaculty)
+    const [selectedDepartment,setSelectedDepartment] = useState("")
+    const [submitted, setSubmitted] = useState(false)
+    const dispatch = useDispatch()
+
+
+    const editFaculty = (data)=>{
+        setFacultyInfoDialog(true)
+        setFaculty(data)
+    }
+    const hideFacultyInfoDialog=()=>{
+        setFacultyInfoDialog(false)
+    }
+    const onUpdateFaculty = async() => {
+        const updatedFaculty = {...faculty, department: selectedDepartment.label}
+        await dispatch(editFacultyServer(updatedFaculty))
+        setFacultyInfoDialog(false)
+      }
+
+    const facultyInfoDialogFooter = (
+        <>
+          <Button label="Update" className="p-button-text" onClick={onUpdateFaculty}/>
+        </>
+      )
+
+    const onDepartmentChange=(e)=>{
+        setSelectedDepartment(e.target.value)
+    }
+
+      
+
+  return (
+    <>
+      <Button
+        icon="pi pi-pencil"
+        className="p-button-rounded p-button-warning mr-2"
+        onClick={() => editFaculty(rowData)}
+      />
+      <Dialog
+        visible={facultyInfoDialog}
+        onHide={hideFacultyInfoDialog}
+        header = "Edit Profile"
+        style={{width:"450px"}}
+        modal
+        className="p-fluid"
+        footer={facultyInfoDialogFooter}
+      >
+        <div className="field">
+          <label htmlFor="name">Name</label>
+          <InputText 
+            id="name"
+            value={faculty.name}
+            required
+            autoFocus
+            className={classNames({"p-invalid":submitted && !faculty.name})}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="institute_id">Institute Mail Id</label>
+          <InputText 
+            id="institute_id"
+            value={faculty.email}
+            required
+            autoFocus
+            className={classNames({"p-invalid":submitted && !faculty.institute_id})}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="department">Department</label>
+          <Dropdown 
+            value={selectedDepartment}
+            options = {departments}
+            placeholder="Select a department"
+            required
+            onChange={onDepartmentChange}
+          />
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default EditFaculty;
---
+++ frontend/src/pages/admin/components/FacultyModel.js
@@ -0,0 +1,91 @@
+import React, { useState, useRef, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import { useDispatch, useSelector } from "react-redux";
+import { getFacultyServer } from "../../../reduxSlices/FacultySlice";
+import DeleteFaculty from "./DeleteFaculty";
+import EditFaculty from "./EditFaculty";
+import Mentees from "./Mentees";
+
+const FacultyModel = () => {
+    const [selectedFaculty, setSelectedFaculty] = useState(null);
+    const dispatch = useDispatch()
+    const reduxFaculty = useSelector(state => state.faculty) 
+
+    if(reduxFaculty.length === 0){
+      dispatch(getFacultyServer())
+    }
+    
+    const actionBodyTemplate = (rowData) => {
+        return (
+          <>
+            <Mentees/>
+            <EditFaculty rowData={rowData}/>
+            <DeleteFaculty rowData={rowData}/>
+          </>
+        );
+      };
+
+
+
+      const paginatorLeft = (
+        <Button type="button" icon="pi pi-refresh" className="p-button-text" />
+      );
+      const paginatorRight = (
+        <Button type="button" icon="pi pi-cloud" className="p-button-text" />
+      );
+
+  return (
+    <>
+    <DataTable
+              value={reduxFaculty}
+              paginator
+              responsiveLayout="scroll"
+              paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+              currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+              rows={10}
+              rowsPerPageOptions={[10, 20, 50]}
+              paginatorLeft={paginatorLeft}
+              paginatorRight={paginatorRight}
+              selection={selectedFaculty}
+              onSelectionChange={(e) => setSelectedFaculty(e.value)}
+            >
+              <Column
+                selectionMode="multiple"
+                headerStyle={{ width: "3rem" }}
+                exportable={false}
+              ></Column>
+              <Column
+                field="name"
+                header="Name"
+                style={{ minWidth: "8rem" }}
+              ></Column>
+              <Column
+                field="email"
+                header="Institute ID"
+                style={{ minWidth: "8rem" }}
+              ></Column>
+              <Column
+                field="department"
+                header="Department"
+                style={{ minidth: "8rem" }}
+              ></Column>
+              <Column
+                field="mentees"
+                header="No. of Mentees"
+                style={{ width: "20%" }}
+              ></Column>
+              <Column
+                header="Actions"
+                body={actionBodyTemplate}
+                exportable={false}
+                style={{ minWidth: "8rem" }}
+              ></Column>
+            </DataTable>
+            
+    </>
+  )
+}
+
+export default FacultyModel
